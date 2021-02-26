dfb62a236487cdff5dc1d432fdd94eb5b71762a5
mihnea-mihai <mihnea.mihai@gmx.com>
2021-02-26 21:33:48 +0200

    add repr

--- wikiword.py
+++ wikiword.py
@@ -1,5 +1,10 @@
 class Wikiword():
 
-    def __init__(word, lang_code):
+    def __init__(self, word, lang_code):
         self.word = word
         self.lang_code = lang_code
+        self.lang = lang_code
+
+    def __repr__(self):
+        return f'{self.lang} {self.word}'
+    
