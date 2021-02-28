e240c861b957948fad49ccb8ae9f44d8f575702c
mihnea-mihai <mihnea.mihai@gmx.com>
2021-02-28 21:30:36 +0200

    add examples for issues

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from wikiword import WikiWord
 while(True):
     print('To exit press Crtl+C.')
     print('Show all descendants of a word: (example: "campus", "la")')
+    print('Tests: Latin eradico, Latin audio, P-Ger gudą')
     lemma = input('Word: ')
     lang_code = input('Lang: ')
     WikiWord(lemma, lang_code).print_descendants()
